d613ea0e12a613d62d019a75d48bb8729df018c7
Alex <alex.salles2007@gmail.com>
2022-11-27 12:49:06 -0300

    Dependencias firestore

---
+++ app/src/main/java/br/edu/infnet/myapplication/ui/home/CadastrarSerieFragment.kt
@@ -0,0 +1,30 @@
+package br.edu.infnet.myapplication.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.edu.infnet.myapplication.R
+import br.edu.infnet.myapplication.databinding.FragmentCadastrarSerieBinding
+
+
+class CadastrarSerieFragment : Fragment() {
+    private var _binding: FragmentCadastrarSerieBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCadastrarSerieBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
