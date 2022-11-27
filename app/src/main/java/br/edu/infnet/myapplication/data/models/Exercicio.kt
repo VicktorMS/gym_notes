8ba99c2db9701ced90f3d6521ece772e893d6b3e
Alex <alex.salles2007@gmail.com>
2022-11-27 12:35:13 -0300

    Deleted some stuff

---
+++ app/src/main/java/br/edu/infnet/myapplication/data/models/Exercicio.kt
@@ -0,0 +1,11 @@
+package br.edu.infnet.myapplication.data.models
+
+data class Exercicio(
+    val id: String = "",
+    val nome: String = "",
+    val peso: Float = 0.0f,
+    val repExercicio: Int = 0,
+    val repMove:Int = 0,
+    val aparelho: String = ""
+
+)
---
+++ app/src/main/java/br/edu/infnet/myapplication/data/models/Serie.kt
@@ -0,0 +1,7 @@
+package br.edu.infnet.myapplication.data.models
+
+data class Serie(
+    val id: String = "",
+    val dia: String = "",
+    val nome: String = ""
+)
